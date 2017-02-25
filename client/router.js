76bc14e0f1dd61faa456296786f059ec649e7d09
Thomas Gerot <tjgerot@gmail.com>
2017-02-25 10:28:43 -0600

    Added routes and small fixes

--- client/router.js
+++ client/router.js
@@ -1,12 +1,31 @@
 // Index route
 FlowRouter.route("/", {
   action: function() {
-    // This statement means:
-    // 'Display the main template and in for the dynamic template called "content", render the template "home".'
+    // Display the main template and in for the dynamic template called "content", render the template "home".
     BlazeLayout.render("main", {
       content: "home"
     });
   },
   // Optional, but it makes me feel warmer inside.
   name: "Index"
+});
+// Survey route
+FlowRouter.route("/survey", {
+  action: function() {
+    // Display the main template and in for the dynamic template called "content", render the template "home".
+    BlazeLayout.render("main", {
+      content: "survey"
+    });
+  },
+  // Again, optional, but it makes me feel warmer inside.
+  name: "Survey"
+});
+// You know the drill
+FlowRouter.route("/dashboard", {
+  action: function() {
+    BlazeLayout.render("main", {
+      content: "dashboard"
+    });
+  },
+  name: "Dashboard"
 });
